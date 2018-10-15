b26da8fda3e9600db8cdcb5a173d3a71148a1c5a
Boris Galicky <boris.galicky@akademiasovy.sk>
2018-10-15 08:32:05 +0200

    init repo

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div class="Title">Hello darkness, my old friend!</div>
+    );
+  }
+}
+
+export default App;
